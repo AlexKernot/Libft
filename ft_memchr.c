53ff31a0a89c81615311b7805fe2685d5c66dc70
Alex Kernot <alex.kernot04@gmail.com>
2023-02-23 14:53:56 +1030

    Add ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akernot <akernot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 14:46:13 by akernot           #+#    #+#             */
+/*   Updated: 2023/02/23 14:53:31 by akernot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*memchr(const void *s, int c, size_t n)
+{
+	unsigned char	to_find;
+	unsigned char	*array;
+	void			*found;
+	size_t			i;
+
+	i = 0;
+	to_find = (unsigned char)c;
+	array = (unsigned char *)s;
+	while (i < n)
+	{
+		if (array[i] == to_find)
+		{
+			found = (void *)&array[i];
+			return (found);
+		}
+		i++;
+	}
+	return (NULL);
+}
