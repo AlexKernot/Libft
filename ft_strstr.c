d6022441dc3230bfed57b4d889290baddb9d47c2
Alex Kernot <alex.kernot04@gmail.com>
2023-02-23 17:24:41 +1030

    Add strstr.c

---
+++ ft_strstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akernot <akernot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 17:12:32 by akernot           #+#    #+#             */
+/*   Updated: 2023/02/23 17:24:33 by akernot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	int		i;
+	int		j;
+	char	*match_start;
+
+	i = 0;
+	j = 0;
+	match_start = (char *)haystack;
+	while (haystack[i] != 0)
+	{
+		if (needle[j] == 0)
+			return (match_start);
+		if (haystack[i] == needle[j])
+		{
+			if (j == 0)
+				match_start = (char *)&haystack[i];
+			j++;
+		}
+		else
+		{
+			j = 0;
+			match_start = (char *)haystack;
+		}
+		i++;
+	}
+	return (NULL);
+}
