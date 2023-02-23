bafaf5881761b267a7e16792d631de08d7acfc08
Alex Kernot <alex.kernot04@gmail.com>
2023-02-23 16:38:40 +1030

    Add ft_strncat.c

---
+++ ft_strncat.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akernot <akernot@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/23 16:19:36 by akernot           #+#    #+#             */
+/*   Updated: 2023/02/23 16:33:24 by akernot          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+size_t	ft_strlen(const char *s);
+
+char	*strncat(char *dst, const char *src, size_t sz)
+{
+	size_t	i;
+	size_t	size;
+
+	i = 0;
+	size = ft_strlen(dst);
+	while (i < sz && src[i] != 0)
+	{
+		dst[size + i] = src[i];
+	}
+	dst[size + i] = 0;
+	return (dst);
+}
