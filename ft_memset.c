e7f29d39688934e00350af3222dac8ab51a0c1c4
Alex Kernot <alex.kernot04@gmail.com>
2023-02-23 13:58:30 +1030

    Rename memset.c to ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memset.c                                           :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akernot <akernot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 13:28:29 by akernot           #+#    #+#             */
-/*   Updated: 2023/02/23 13:39:32 by akernot          ###   ########.fr       */
+/*   Updated: 2023/02/23 13:47:07 by akernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	*memset(void *s, int c, size_t n)
+void	*ft_memset(void *s, int c, size_t n)
 {
 	int		*pointer;
 	size_t	i;
