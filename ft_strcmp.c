2ac86a528196d39a8c7be14ddcd1490bf3e8f0a9
AlexKernot <alex.kernot74@gmail.com>
2023-03-05 17:20:08 +1030

    Cast to unsigned char and check for empty strings

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -13,12 +13,18 @@
 int	ft_strcmp(const char *s1, const char *s2)
 {
 	int	i;
+	const unsigned char *c_s1;
+	const unsigned char *c_s2;
 
 	i = 0;
-	while (s1[i] != 0 && s2[i] != 0)
+	c_s1 = (const unsigned char *)s1;
+	c_s2 = (const unsigned char *)s2;
+	if (*s1 == 0 || *s2 == 0)
+		return (*c_s1 - *c_s2);
+	while (c_s1[i] != 0 && c_s2[i] != 0)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (c_s1[i] != c_s2[i])
+			return (c_s1[i] - c_s2[i]);
 		i++;
 	}
 	return (0);
