58212cb29469200099251e1c1699bc8ed8aa24e1
Alex Kernot <alex.kernot04@gmail.com>
2023-03-05 13:43:42 +1030

    Add logic for null inputs and comparing unsigned chars

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: akernot <akernot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 18:00:38 by akernot           #+#    #+#             */
-/*   Updated: 2023/02/27 09:09:05 by akernot          ###   ########.fr       */
+/*   Updated: 2023/03/04 22:57:33 by akernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,14 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n || (s1[i] != 0 && s2[i] != 0))
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	if (*s1 == 0 || *s2 == 0 || n == 0)
+		return (0);
+	while (i < n && s1[i] != 0 && s2[i] != 0)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
