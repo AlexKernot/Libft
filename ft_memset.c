dc045465530667bcf7d772f91aeef33bba0d2794
Alex Kernot <alex.kernot04@gmail.com>
2023-03-05 13:42:52 +1030

    Change int to unsigned char

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: akernot <akernot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 13:28:29 by akernot           #+#    #+#             */
-/*   Updated: 2023/02/23 13:47:07 by akernot          ###   ########.fr       */
+/*   Updated: 2023/03/04 21:45:13 by akernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	int		*pointer;
+	unsigned char		*pointer;
 	size_t	i;
 
 	i = 0;
-	pointer = (int *)s;
-	while (i < n)
+	pointer = (unsigned char *)s;
+	while (i < n && pointer[i] != 0)
 	{
 		pointer[i] = c;
 		i++;
