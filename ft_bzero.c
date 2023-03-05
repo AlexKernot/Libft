bc14750c66c28c2f575237268285b768008ce0e0
Alex Kernot <alex.kernot04@gmail.com>
2023-03-05 13:39:31 +1030

    Add logic for null char and use unsigned chars

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: akernot <akernot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 13:41:16 by akernot           #+#    #+#             */
-/*   Updated: 2023/02/23 13:46:38 by akernot          ###   ########.fr       */
+/*   Updated: 2023/03/04 21:57:55 by akernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	int		*pointer;
+	unsigned char		*pointer;
 	size_t	i;
 
-	pointer = (int *)s;
+	pointer = (unsigned char *)s;
 	i = 0;
-	while (i < n)
+	while (i < n && pointer[i] != 0)
 	{
 		pointer[i] = 0;
 		i++;
