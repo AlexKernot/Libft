4aa395eeaacf025027a66a8b15d05dfd273b9c2f
Alex Kernot <alex.kernot04@gmail.com>
2023-03-05 13:44:32 +1030

    Add null logic, free malloced mem, and check null chars

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: akernot <akernot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 14:33:12 by akernot           #+#    #+#             */
-/*   Updated: 2023/02/23 14:43:36 by akernot          ###   ########.fr       */
+/*   Updated: 2023/03/04 22:43:27 by akernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,13 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	int		*int_dest;
 	int		*int_src;
 
+	if (src == NULL || dest == NULL || n == 0)
+		return (NULL);
 	i = 0;
 	int_dest = (int *)dest;
 	int_src = (int *)src;
 	temp_array = (int *)malloc(sizeof(int) * n);
-	while (i < n)
+	while (i < n && int_src[i] != 0)
 	{
 		temp_array[i] = int_src[i];
 		i++;
@@ -33,9 +35,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		int_dest[i] = int_dest[i];
+		int_dest[i] = temp_array[i];
 		i++;
 	}
 	int_dest[i] = 0;
+	free(temp_array);
 	return (dest);
 }
