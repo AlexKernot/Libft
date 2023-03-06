6ff9c065f2643beaee565aba73a30d6929c6d294
Alex Kernot <alex.kernot04@gmail.com>
2023-03-06 18:57:34 +1030

    Add logic for seperator string

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -16,23 +16,35 @@
 int		ft_strlen(char *str);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 
-char	*ft_strtrim(const char *s)
+static int	check_seperator(const char c, const char *sep)
 {
-	char *new_string;
-	int	length;
-	int	start;
-	int	end;
+	int	i;
+
+	i = 0;
+	while (sep[i] != 0)
+	{
+		if (c == sep[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(const char *s, const char *sep)
+{
+	char	*new_string;
+	int		length;
+	int		start;
+	int		end;
 
 	if (s == NULL)
 		return (NULL);
 	length = ft_strlen((char *)s);
 	start = 0;
-	while (s[start] == ' ' || s[start] == '\n'
-		|| s[start] == '\t' || start < length)
+	while (check_seperator(s[start], sep))
 		start++;
 	end = length - 1;
-	while (s[end] == ' ' || s[end] == '\n'
-		|| s[end] == '\t' || end > 0)
+	while (check_seperator(s[start], sep))
 		end--;
 	length = end - start;
 	new_string = (char *)malloc(length * sizeof(char) + 1);
